040a641c331a3ea11bb00e6f7c9b73454d6daed4
Ali Azhari <Aliazhari@hotmail.com>
2022-10-09 16:12:10 -0400

    Adding comment

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from rembg import remove
 from PIL import Image
 
-input_path = 'ali1.png'
-output_path = 'ali.png'
+# make sure you have the picture "old_pic.jpg" in your folder
+input_path = 'old_pic.jpg'
+output_path = 'new_pic.png'
 input = Image.open(input_path)
 output = remove(input)
 output.save(output_path)
